8810c72cf8e028771ec7f052f1b3445d47506eea
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-01-31 13:13:59 +0200

    ✔ ViewStart cshtml in ASP NET Core MVC changes !

--- DanEmployeeManagement/Startup.cs
+++ DanEmployeeManagement/Startup.cs
@@ -15,8 +15,6 @@ namespace DanEmployeeManagement
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         private IConfiguration config;
 
         public Startup(IConfiguration config)
@@ -26,8 +24,6 @@ namespace DanEmployeeManagement
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddMvcCore(options => options.EnableEndpointRouting = false).AddXmlSerializerFormatters();
-
             services.AddMvc(mvcoptions => mvcoptions.EnableEndpointRouting = false)
                 .AddXmlSerializerFormatters()
                 .AddXmlDataContractSerializerFormatters();
@@ -35,7 +31,6 @@ namespace DanEmployeeManagement
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -54,18 +49,3 @@ namespace DanEmployeeManagement
         }
     }
 }
-// To add foo file like deff 
-//FileServerOptions fileServerOptions = new FileServerOptions();
-//fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
-//fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
-//app.UseFileServer(fileServerOptions);
-
-
-// How to show deff files in web browser and how to change other file to deff !
-//DefaultFilesOptions deffFilesOption = new DefaultFilesOptions();
-//deffFilesOption.DefaultFileNames.Clear();
-//deffFilesOption.DefaultFileNames.Add("foo.html");
-//app.UseDefaultFiles(deffFilesOption);
-
-//// To vizualize images !
-//app.UseStaticFiles();
