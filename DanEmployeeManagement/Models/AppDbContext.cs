24fb9db95be4704703507ec68df686bf9db4f23d
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-02-06 19:50:43 +0200

    ✔ DbContext in entity framework core changes !

---
+++ DanEmployeeManagement/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DanEmployeeManagement.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
