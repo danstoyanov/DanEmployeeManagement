56e177755e43d505395ee888105ded9c0d50efb2
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-02-07 12:25:46 +0200

    ✔ Keeping domain models and database schema in sync in asp net core changes !

--- DanEmployeeManagement/Models/Employee.cs
+++ DanEmployeeManagement/Models/Employee.cs
@@ -15,12 +15,14 @@ namespace DanEmployeeManagement.Models
         public string Name { get; set; }
 
         [Required]
-        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", 
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
             ErrorMessage = "Invalid emails format !")]
         [Display(Name = "Office Email")]
         public string Email { get; set; }
 
         [Required]
         public Depts Department { get; set; }
+
+        public string PhotoPath { get; set; }
     }
 }
