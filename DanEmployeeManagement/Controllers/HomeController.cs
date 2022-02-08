670e3993d5a7ae7965b7821ad0553dbdc2d32848
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-02-08 16:06:08 +0200

    ✔ Httppost edit action in asp net core mvc changes !

--- DanEmployeeManagement/Controllers/HomeController.cs
+++ DanEmployeeManagement/Controllers/HomeController.cs
@@ -64,26 +64,68 @@ namespace DanEmployeeManagement.Controllers
         }
 
         [HttpPost]
+        public IActionResult Edit(EmployeeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = this.employeeRepository.GetEmployee(model.Id);
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+
+                if (model.Photos != null)
+                {
+                    if (model.ExistingPhotoPath != null)
+                    {
+                        var filePath = Path.Combine(
+                            hostingEnvironment.WebRootPath,
+                            "images",
+                            model.ExistingPhotoPath);
+
+                        System.IO.File.Delete(filePath);
+                    }
+
+                    employee.PhotoPath = ProccesUploadedFile(model);
+                }
+
+                this.employeeRepository.Update(employee);
+
+                return RedirectToAction("index");
+            }
+
+            return View();
+        }
+
+        private string ProccesUploadedFile(EmployeeCreateViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if (model.Photos != null && model.Photos.Count > 0)
+            {
+                foreach (IFormFile photo in model.Photos)
+                {
+                    string uploadsFolder = Path.Combine(this.hostingEnvironment.WebRootPath, "images");
+
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        photo.CopyTo(fileStream);
+                    }
+                }
+            }
+
+            return uniqueFileName;
+        }
+
+        [HttpPost]
         public IActionResult Create(EmployeeCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
-                string uniqueFileName = null;
-
-                if (model.Photos != null && model.Photos.Count > 0)
-                {
-                    foreach (IFormFile photo in model.Photos)
-                    {
-                        string uploadsFolder = Path.Combine(this.hostingEnvironment.WebRootPath, "images");
-
-                        uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
-
-                        string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-
-                        photo.CopyTo(new FileStream(filePath, FileMode.Create));
-                    }
-                }
-
+                string uniqueFileName = ProccesUploadedFile(model);
 
                 var newEmployee = new Employee
                 {
